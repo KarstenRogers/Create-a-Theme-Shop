633d0ee32c56d76f283adeb8a64a3e551b1aca6b
Karsten Rogers <karstenrogers@Karstens-MacBook-Air.local>
2019-02-18 10:13:36 -0700

    Setup a Page to Display all the Products

---
+++ wp-content/themes/bootstrapwp/archive-download.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The template for displaying all downloads.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package bootstrapwp
+ */
+
+get_header(); ?>
+
+<div class="container">
+	<div class="row">
+		<div id="primary" class="col-lg-12">
+			<main id="main" class="site-main" role="main">
+				<div class="row">
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'content', 'downloads' ); ?>
+
+				<?php
+					// If comments are open or we have at least one comment, load up the comment template
+				if ( comments_open() || '0' != get_comments_number() ) :
+					comments_template();
+				endif;
+				?>
+
+			<?php endwhile; // end of the loop. ?>
+			</div>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div>
+</div>
+
+
+<?php get_footer(); ?>
